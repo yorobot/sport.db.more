bd5eac039882f128d47b825867558cfe66c08a7a
Gerald Bauer <gerald.bauer@gmail.com>
2024-08-28 18:45:01 +0200

    up

--- footballdata/up/helper.rb
+++ footballdata/up/helper.rb
@@ -1,12 +1,19 @@
-$LOAD_PATH.unshift( '../../../sportdb/sport.db/sportdb-langs/lib' )
+$LOAD_PATH.unshift( '../../../sportdb/sport.db/parser/lib' )
 $LOAD_PATH.unshift( '../../../sportdb/sport.db/sportdb-structs/lib' )
-$LOAD_PATH.unshift( '../../../sportdb/sport.db/sportdb-formats/lib' )
+$LOAD_PATH.unshift( '../../../sportdb/sport.db/sportdb-quick/lib' )
 $LOAD_PATH.unshift( '../../../sportdb/sport.db/sportdb-catalogs/lib' )
+$LOAD_PATH.unshift( '../../../sportdb/sport.db/sportdb-search/lib' )
+$LOAD_PATH.unshift( '../../../sportdb/sport.db/sportdb-formats/lib' )
 $LOAD_PATH.unshift( '../sportdb-writers/lib' )
 $LOAD_PATH.unshift( '../sportdb-linters/lib' )
 
-require 'sportdb/catalogs'
+
+require 'sportdb/formats'
 require 'sportdb/writers'   ## note - requires sportdb/formats only (require sportdb/catalogs first)!!!
+
+
+load_env   ## use dotenv (.env)
+
 
 
 $LOAD_PATH.unshift( './lib' )
@@ -22,18 +29,18 @@ Club    = Sports::Club
 
 SportDb::Import.config.catalog_path = '../../../sportdb/sport.db/catalog/catalog.db'
 
-## move (for reusue) to CatalogDb::Metal.tables or such - why? 
+## move (for reusue) to CatalogDb::Metal.tables or such - why?
 CatalogDb::Metal.tables
 
 
 ##########
 ##  webcache settings
-Webcache.root = '/sports/cache'  
+Webcache.root = '/sports/cache'
 pp File.expand_path( Webcache.root )
 #=> /sports/cache
 
 ## max. 10 requests/minute
-##          about ~6 request/minute (if delay is 10 secs) 
+##          about ~6 request/minute (if delay is 10 secs)
 Webget.config.sleep  = 10
 
 #########
